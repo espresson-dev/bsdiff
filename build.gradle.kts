e7c061093d18b58fe357d991378b66bc1e1c434e
hzh <565150953@qq.com>
2025-04-19 18:13:20 +0800

    1

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.xeonyu.library)
+    alias(libs.plugins.vanniktechPublish)
+}
+
+android {
+    namespace = "com.yzq.bsdiff"
+
+    defaultConfig {
+        externalNativeBuild {
+            cmake {
+                cppFlags("")
+            }
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+}
